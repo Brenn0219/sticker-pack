9da2201f706028981586cdab21510f05d5aabb43
brenn0219 <brennoaugusto2109@gmail.com>
2023-08-03 12:01:14 -0300

    feat: show sent message

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 let subtract = document.getElementById("subtract");
 let add = document.getElementById("add");
 let amountOfStickers = document.getElementById("amount");
+let buttonSend = document.getElementById("send");
 
 subtract.addEventListener("click", subtractSticker);
 add.addEventListener("click", addSicker);
+buttonSend.addEventListener("click", menssageSend);
 
+// function to subtract when clicking on the button
 function subtractSticker() {
     if (amountOfStickers.value == "" || amountOfStickers.value == "0") {
-        amountOfStickers.classList.add("button-error");
+        amountOfStickers.classList.add("error");
         subtract.classList.add("button-desabled");
     } else {
         amountOfStickers.value = parseInt(amountOfStickers.value) - 1;
     }
 }
 
+// function to add when clicking the button
 function addSicker() {
-    if (amountOfStickers.className == "button-error") {
-        amountOfStickers.classList.remove("button-error");
+    if (subtract.classList.contains("button-desabled")) {
+        subtract.classList.remove("button-desabled");
+        amountOfStickers.classList.remove("error")
     }
 
     if (amountOfStickers.value == "") {
@@ -24,4 +29,11 @@ function addSicker() {
     } else {
         amountOfStickers.value = parseInt(amountOfStickers.value) + 1;
     }
+}
+
+// function to show sent message
+function menssageSend() {
+    let menssage = document.getElementById("menssage-sent");
+    menssage.innerHTML = "Formulário enviado com sucesso!";
+    buttonSend.classList.add("button-desabled");
 }
